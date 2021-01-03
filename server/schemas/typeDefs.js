74898aad6d87d4cb3b1ee9bce92402a4cfda299a
ry brown <ryan.alex.brown@gmail.com>
2021-01-02 20:39:30 -0700

    basic graphql

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,16 @@
-// import the gql tagged template function
-const {gql} = require('apollo-server-express');
+const { gql } = require('apollo-server-express');
 
-//create our typeDefs
 const typeDefs = gql`
-type Thought {
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
+  type Thought {
     _id: ID
     thoughtText: String
     createdAt: String
@@ -12,16 +19,19 @@ type Thought {
     reactions: [Reaction]
   }
 
-type Reaction{
+  type Reaction {
     _id: ID
     reactionBody: String
     createdAt: String
     username: String
-}
-    type Query {
-        thoughts(username: String): [Thought]
-    }
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
 `;
 
-//export the typeDefs
 module.exports = typeDefs;
